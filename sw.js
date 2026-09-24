70247a79104ef24085f675bc3c6c7829002c2f85
SandovalJon <SandovalJon@users.noreply.github.com>
2026-09-23 21:04:46 -0600

    Add update button and auto-detect new versions

--- sw.js
+++ sw.js
@@ -25,6 +25,12 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
+self.addEventListener('message', e => {
+  if (e.data && e.data.skipWaiting) {
+    self.skipWaiting();
+  }
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
